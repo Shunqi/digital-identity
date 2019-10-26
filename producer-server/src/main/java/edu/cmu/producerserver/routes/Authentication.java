b2b2b501e94c7d25855220bb45e7c7118c6e0c3b
Vinit Shah <vinit.shah298@gmail.com>
2019-10-26 11:48:18 -0400

    Improves the challenge route
     - adds functionalities to decrypt and encrypt data
     - adds producer-keys to be used by others in the application
    
    Signed-off-by: Vinit Shah <vinit.shah298@gmail.com>

--- producer-server/src/main/java/edu/cmu/producerserver/routes/Authentication.java
+++ producer-server/src/main/java/edu/cmu/producerserver/routes/Authentication.java
@@ -1,5 +1,6 @@
 package edu.cmu.producerserver.routes;
 
+import edu.cmu.producerserver.security.Security;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,17 +9,64 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.simple.JSONObject;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 @RestController
 public class Authentication {
+
+    String appURL = "http://10.0.0.153:8081";
+    Security sec = new Security();
 
     @RequestMapping(
             value = "/authentication/challenge",
             method = RequestMethod.POST,
             consumes = "text/plain"
     )
-    public String challenge(@RequestBody String payload) throws ParseException {
+    public String challenge(@RequestBody String payload, HttpServletResponse response) throws ParseException {
         JSONParser parser = new JSONParser();
         JSONObject json = (JSONObject) parser.parse(payload);
+
+        // Decrypt the string received and get the challenge value
+
+        // Make call to the app
+        try {
+            URL url = new URL(appURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if(conn.getResponseCode() == 200) {
+                // Generate auth token and store in redis
+                //cresponse.getWriter().write(json.toJSONString());
+            } else if(conn.getResponseCode() == 401) {
+                // Send back saying unauthorized
+            }
+
+            PublicKey publicKey = sec.readPublicKey("src/main/keys/public.der");
+            PrivateKey privateKey = sec.readPrivateKey("src/main/keys/private.der");
+            byte[] message = "Hello World from ".getBytes("UTF8");
+            byte[] secret = sec.encrypt(publicKey, message);
+            byte[] recovered_message = sec.decrypt(privateKey, secret);
+            System.out.println(new String(recovered_message, "UTF8"));
+
+            conn.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         System.out.println(json.get("name"));
         return "HEllo World";
     }
