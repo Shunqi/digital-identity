6f3cf52cfc2e7d9b76a8fc4bc03be8b30a3bdfef
poojithaPrasad94 <pooji2201@gmail.com>
2019-11-03 17:41:39 -0500

    Add producer model

---
+++ consumer-server/src/main/java/edu/cmu/consumerserver/model/Producer.java
@@ -0,0 +1,18 @@
+package edu.cmu.consumerserver.model;
+
+public class Producer {
+
+    private String did;
+
+    public Producer(String did) {
+        this.did = did;
+    }
+
+    public String getDid() {
+        return did;
+    }
+
+    public void setDid(String did) {
+        this.did = did;
+    }
+}
