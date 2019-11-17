416faaded0882f99b37e438978460953350dd815
Shunqi Wang <wangshunqi2@gmail.com>
2019-11-17 16:22:36 -0500

    Add checking for authtoken

--- producer-server/src/main/java/edu/cmu/producerserver/controller/GetDataController.java
+++ producer-server/src/main/java/edu/cmu/producerserver/controller/GetDataController.java
@@ -2,28 +2,43 @@ package edu.cmu.producerserver.controller;
 
 import edu.cmu.producerserver.model.ConsumerData;
 import edu.cmu.producerserver.repository.ConsumerDataRepository;
+import edu.cmu.producerserver.service.RedisService;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/data")
 public class GetDataController {
 
     private final ConsumerDataRepository consumerDataRepository;
+    private final RedisService redisClient;
 
-    public GetDataController(ConsumerDataRepository consumerDataRepository) {
+    public GetDataController(ConsumerDataRepository consumerDataRepository, RedisService redisClient) {
         this.consumerDataRepository = consumerDataRepository;
+        this.redisClient = redisClient;
     }
 
     @ResponseBody
     @GetMapping("/{type}")
-    String getData(@PathVariable String type) {
-        ConsumerData consumerData = consumerDataRepository.findByType(type);
+    String getData(@PathVariable String type, @RequestParam(name="authtoken") String authtoken,
+                 HttpServletResponse response) {
+        String data = null;
 
-        String result = null;
-        if (consumerData != null) {
-            result = consumerData.getData();
+        // check authtoken
+        String DID = redisClient.getValue(authtoken);
+        if (DID == null) {
+            response.setStatus(401);
+            return null;
         }
 
-        return result;
+        // check permission
+
+        ConsumerData consumerData = consumerDataRepository.findByType(type);
+
+        if (consumerData != null) {
+            data = consumerData.getData();
+        }
+        return data;
     }
 }
